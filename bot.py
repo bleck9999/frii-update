2003628b6fe16bc26a89898656db3c66568acba6
bleck9999 <55853712+bleck9999@users.noreply.github.com>
2020-09-02 14:31:52 +0100

    do not nearly enough

--- bot.py
+++ bot.py
@@ -1,13 +1,31 @@
 import configparser
 from discord.ext import commands
-
-
-class FriiUpdate(commands.Bot):
-    def __init__(self):
-        self.load_extension("cogs.friiUpdate")
-
+from traceback import format_exception
 
 config = configparser.ConfigParser()
 config.read("frii_update.ini")
+
+
+class FriiUpdate(commands.Bot):
+    def __init__(self, command_prefix, **options):
+        super().__init__(command_prefix, **options)
+        self.load_extension("cogs.friiUpdate")
+        self.role = int(config["Config"]["Role ID"])
+
+    # Exception handling modified from nh-server/Kurisu
+    # Licensed under apache 2 (https://www.apache.org/licenses/LICENSE-2.0)
+    async def on_command_error(self, ctx, exception):
+        self.channel = await self.fetch_channel(int(config["Config"]["Channel ID"]))
+        await self.channel.send(f"<@&{self.role} an unhandled exception has occurred")
+        exc = getattr(exception, 'original', exception)
+        msg = "".join(format_exception(type(exc), exc, exc.__traceback__))
+        error_paginator = commands.Paginator()
+        for chunk in [msg[i:i + 1800] for i in range(0, len(msg), 1800)]:
+            error_paginator.add_line(chunk)
+        for page in error_paginator.pages:
+            await self.channel.send(page)
+
+
 bot = FriiUpdate(command_prefix=".")
+print("Run bot")
 bot.run(config["Tokens"]["Discord"])
--- cogs/friiUpdate.py
+++ cogs/friiUpdate.py
@@ -1,11 +1,22 @@
+import asyncio
 import configparser
 import discord
+import git
 from discord.ext import commands
 from github import Github
 
 # format for entry: [Repo Path (str), Embed Colour (discord.Color)]
 repos = [
-
+    ["/media/blecc/storageBecause/switchhax2/Atmosphere", discord.Color(0x00a6ff)],
+    ["/media/blecc/storageBecause/switchhax2/hekate", discord.Color(0xff0000)],
+    ["/media/blecc/storageBecause/switchhax2/kurisu", discord.Color(0x8000ff)],
+    ["/media/blecc/storageBecause/switchhax2/switch-guide", discord.Color(0x272B30)],
+    ["/media/blecc/storageBecause/switchhax2/Lockpick_RCM", discord.Color(0xffff00)],
+    ["/media/blecc/storageBecause/switchhax2/emmchaccgen", discord.Color(0x005200)],
+    ["/media/blecc/storageBecause/switchhax2/GUIModManager", discord.Color(0x00ff00)],
+    ["/media/blecc/storageBecause/switchhax2/TegraExplorer", discord.Color(0xba5d00)],
+    ["/media/blecc/storageBecause/switchhax2/TegraScript", discord.Color(0xba5d00)],
+    ["/media/blecc/storageBecause/switchhax2/themezer-nx", discord.Color(0x27aad6)]
 ]
 
 
@@ -14,9 +25,26 @@ class Loop(commands.Cog):
         conf = configparser.ConfigParser()
         conf.read("frii_update.ini")
         self.bot = bot
-        self.channel = bot.fetch_channel(conf["Config"]["Channel ID"])
-        self.role = bot.fetch_role(conf["Config"]["Channel ID"])
+        self.channel = int(conf["Config"]["Channel ID"])
+        self.role = int(conf["Config"]["Channel ID"])
+        self.ghAPI = Github(conf["Tokens"]["Github"])
 
+        # asyncio.run(self.updateLoop())
+
+    async def updateLoop(self):
+        await self.bot.wait_until_ready()
+        #channel = await self.bot.fetch_channel(self.channel)
+        while True:
+            for i in range(len(repos)):
+                repo = git.Repo(repos[i][0])
+                NEW_COMMITS = {}
+                fetchinfo = repo.git.fetch()
+
+                # for branch in repo.branches:
+
+    @commands.command()
+    async def startLoop(self, ctx):
+        await self.updateLoop()
 
 
 def setup(bot):
