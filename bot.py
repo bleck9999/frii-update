1d0fa4c0cbf02bffe9fa3d3b916f656d336f72b4
bleck9999 <55853712+bleck9999@users.noreply.github.com>
2020-09-02 12:18:30 +0100

    start rewrite

--- bot.py
+++ bot.py
@@ -1,24 +1,13 @@
-import time
+import configparser
 from discord.ext import commands
 
 
-class friiUpdate(commands.Bot):
-
-    def __init__(self, command_prefix):
-        super().__init__(command_prefix=command_prefix)
-
-    def load_cog(self, extension):
-        self.load_extension(extension)
+class FriiUpdate(commands.Bot):
+    def __init__(self):
+        self.load_extension("cogs.friiUpdate")
 
 
-token = open("discord_token.txt").readline()
-
-print("Connecting to P.U.T.I.N. network...")
-time.sleep(1)
-print("Connected!")
-
-bot = friiUpdate('.')
-print("Load cog...")
-bot.load_cog("cogs.friiUpdate")
-print("Run bot...")
-bot.run(token)
+config = configparser.ConfigParser()
+config.read("frii_update.ini")
+bot = FriiUpdate(command_prefix=".")
+bot.run(config["Tokens"]["Discord"])
